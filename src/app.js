9fc261d102f532a30b6498c3c2b7c0ae63bb2b8e
robert daniel motisan <robertmotisan@gmail.com>
2026-01-09 23:53:01 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
-import "bootstrap";
-import "./style.css";
+window.onload = function () {
 
+let who = ['Mi madre', 'El ascensor', 'Un repartidor', 'Un vecino'];
+let action = ['me llamo', 'tardo un eternidad', 'me paro', 'se quedo parado'];
+let what = ['para pedirme un favor', 'sin razon aparente', 'porque necesitaba algo'];
+let when = ['en el garaje.', 'justo cuando iba a salir.', 'al llegar al coche.', 'al salir del edificio.', 'al salir.'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let ex1 =Math.floor(Math.random() * who.length);
+let ex2 =Math.floor(Math.random() * action.length);
+let ex3 =Math.floor(Math.random() * what.length);
+let ex4 =Math.floor(Math.random() * when.length);
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+document.querySelector("#excuse").innerHTML =( who[ex1] + " " + action[ex2] + " " + what[ex3] + " " + when[ex4] );
+}
