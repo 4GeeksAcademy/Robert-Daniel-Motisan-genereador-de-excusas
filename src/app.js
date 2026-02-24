1d827ba4362e2683fca1a2cd8d77394a98d8bc96
Robert Daniel Motisan <robertmotisan@gmail.com>
2026-02-24 20:00:01 +0000

    areglo del codigo y simplificacion

--- src/app.js
+++ src/app.js
@@ -1,14 +1,19 @@
 window.onload = function () {
 
-let who = ['Mi madre', 'El ascensor', 'Un repartidor', 'Un vecino'];
-let action = ['me llamo', 'tardo un eternidad', 'me paro', 'se quedo parado'];
-let what = ['para pedirme un favor', 'sin razon aparente', 'porque necesitaba algo'];
-let when = ['en el garaje.', 'justo cuando iba a salir.', 'al llegar al coche.', 'al salir del edificio.', 'al salir.'];
-
-let ex1 =Math.floor(Math.random() * who.length);
-let ex2 =Math.floor(Math.random() * action.length);
-let ex3 =Math.floor(Math.random() * what.length);
-let ex4 =Math.floor(Math.random() * when.length);
-
-document.querySelector("#excuse").innerHTML =( who[ex1] + " " + action[ex2] + " " + what[ex3] + " " + when[ex4] );
+    const who = ['Mi madre', 'El ascensor', 'Un repartidor', 'Un vecino'];
+    const action = ['me llamo', 'tardo un eternidad', 'me paro', 'se quedo parado'];
+    const what = ['para pedirme un favor', 'sin razon aparente', 'porque necesitaba algo'];
+    const when = ['en el garaje.', 'justo cuando iba a salir.', 'al llegar al coche.', 'al salir del edificio.', 'al salir.'];
+    //funcion para generar numeros aleatorios
+    const randomNumGenerator = (ArrayforUse) => Math.floor(Math.random() * ArrayforUse.length)
+    //funcion para extraer valor del array
+    const getRandomValueFromArray = (ArrayforUse) => ArrayforUse[randomNumGenerator(ArrayforUse)]
+    //array de las "variables"
+    const nouns = [who, action, what, when];
+    // genera la excusa
+    const excuse = nouns.reduce((acc, array) => {
+        return acc + getRandomValueFromArray(array) + " "
+    }, "");
+    //excusa generada en el html
+    document.querySelector("#excuse").innerHTML = (excuse);
 }
